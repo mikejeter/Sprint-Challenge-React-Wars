3a6628baec61ae42e0b0fa8b94dec2490e5e53a9
mikejeter <mjeter45@yahoo.com>
2019-09-29 23:37:14 -0400

    modified PeopleList.js, LukeSkywalker.js, App.js

--- starwars/src/components/LukeSkywalker.js
+++ starwars/src/components/LukeSkywalker.js
@@ -12,7 +12,6 @@ export default function LukeSkywalker(props) {
         <CardBody>
           <CardTitle>{props.name}</CardTitle>
         </CardBody>
-        <img width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
           <CardText>{props.height}</CardText>
           <CardText>{props.mass}</CardText>
@@ -22,13 +21,13 @@ export default function LukeSkywalker(props) {
           <CardText>{props.birth_year}</CardText>
           <CardText>{props.gender}</CardText>
           <CardLink href={props.homeworld}></CardLink>
-          <CardLink href={props.films.Array[5]}></CardLink>
+          <CardLink href={props.films}></CardLink>
           <CardLink href={props.species}></CardLink>
-          <CardLink href={props.vehicles.Array[2]}></CardLink>
-          <CardLink href={props.starships.Array[2]}></CardLink>
-          <CardText>{props.birth_year}</CardText>
-          <CardText>{props.birth_year}</CardText>
-          <CardLink href={props.species}></CardLink>
+          <CardLink href={props.vehicles}></CardLink>
+          <CardLink href={props.starships}></CardLink>
+          <CardText>{props.created}</CardText>
+          <CardText>{props.edited}</CardText>
+          <img src={`${props.url}`} alt="" />
         </CardBody>
       </Card>
       </Col>
---
+++ starwars/src/components/PeopleList.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Col } from "reactstrap";
+import LukeSkywalker from "./LukeSkywalker.js";
+
+
+
+export default function PeopleList(props) {
+
+  const [people, setPeople] = useState([]);
+//   const [api_key] = "cy0qFcpy5DbERTfHTRT1Mv7j6vtiv6kcUht9BF0o";
+// console.log(copyright,"this is my initial state");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const peopleList = await axios.get(
+          "https://swapi.co/api/people/1/"
+        );
+        // 12
+        
+        setPeople(peopleList.data);
+      } catch (err) {
+        console.log(err);
+      } //try block
+    } // fetchData()
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <Col>
+      <LukeSkywalker
+        people={people.people}
+        height={people.height}
+        mass={people.mass}
+        hair_color={people.hair_color}
+        skin_color={people.skin_color}
+        eye_color={people.eye_color}
+        birth_year={people.birth_year}
+        gender={people.gender}
+        homeworld={people.homeworld}
+        films={people.films}
+        species={people.species}
+        vehicles={people.vehicles}
+        starships={people.starships}
+        created={people.created}
+        edited={people.edited}
+        url={people.url}
+      />
+      </Col>
+    </Container>
+  );
+}
